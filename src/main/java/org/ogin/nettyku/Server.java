9f4d842579f2f09cb3dccd500bc70259439bc9b5
unknown <okta.ginting@gmail.com>
2015-01-14 06:56:35 +0700

    cleanup package

--- src/main/java/org/ogin/nettyku/Server.java
+++ src/main/java/org/ogin/nettyku/Server.java
@@ -1,7 +1,6 @@
 package org.ogin.nettyku;
 
 import org.ogin.nettyku.connector.Connector;
-import org.ogin.nettyku.protocol.discard.DiscardProvider;
 
 /**
  * Created by Jabrik on 25/11/2014.
--- src/main/java/org/ogin/nettyku/protocol/discard/DiscardInitializer.java
+++ src/main/java/org/ogin/nettyku/protocol/discard/DiscardInitializer.java
@@ -3,11 +3,6 @@ package org.ogin.nettyku.protocol.discard;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-import org.ogin.nettyku.protocol.discard.DiscardServerHandler;
 
 
 /**
